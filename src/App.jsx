39cffdaffdaa2192ba9a1834f09bc4e1c698bc21
william <willshinji@gmail.com>
2019-08-17 13:31:38 -0300

    initial sketch

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import React, {Component} from 'react'
 import './App.css'
 
 class App extends Component {
+    state = {value: 0, isStarted: false}
+    
+    start = () => {
+        const {counter} = this
+        const {isStarted} = this.state
+
+        if (isStarted) return        
+        setInterval(() => counter(), 1000)
+    }
+    
+    counter = () => {
+        const {value} = this.state
+        this.setState({value: value + 1, isStarted: true})
+    }
+
     render () {
-        return (<div className='stopwatcher'>00:02:00</div>) 
+        const {value, isStarted} = this.state
+        const {start} = this
+        return (
+            <>
+                <div className='stopwatcher'>{value}</div>
+                <button onClick={start} disabled={isStarted}>Start</button>
+            </>
+        ) 
     }
 }
 export default App
