3d84b835ca287ffd72405745d4aef37b845ae83a
william <willshinji@gmail.com>
2019-09-19 13:39:36 -0300

    clear and stop features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React, {Component} from 'react'
 import './App.css'
 
+const INITIAL_VALUES = {seconds: 0, minutes: 0, hours: 0, isStarted: false, counterId: 0} 
 class App extends Component {
-    state = {seconds: 0, minutes: 0, hours: 0, isStarted: false}
+    
+    state = INITIAL_VALUES
     
     start = () => {
         const {counter} = this
         const {isStarted} = this.state
-
+        let counterId
+        
         if (isStarted) return        
-        setInterval(() => counter(), 1000)
+        counterId = setInterval(() => counter(), 1000)
+        this.setState({...this.state, counterId})
     }
     
     counter = () => {
@@ -26,22 +30,37 @@ class App extends Component {
         this.setState({seconds, minutes, hours, isStarted: true})
     }
 
+    clear = () => {
+        this.stop(this.state.counterId)
+        this.setState(INITIAL_VALUES)
+    }
+
+    stop = counterId => {
+        clearInterval(counterId);
+    }
+
     formatTime = number => {
         return (number ? (number > 9 ? number : "0" + number) : "00")
     }
 
     render () {
-        const {seconds, minutes, hours, isStarted} = this.state
-        const {start, formatTime} = this
+        const {seconds, minutes, hours, isStarted, counterId} = this.state
+        const {start, stop, clear, formatTime} = this
         return (
             <>
                 <div className='stopwatcher'>
                     {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}
                 </div>
                 <div className='buttons'>
-                    <button onClick={start} disabled={isStarted}>Start</button>
-                    <button>Stop</button>
-                    <button>Clear</button>
+                    <button onClick={start} disabled={isStarted}>
+                        Start
+                    </button>
+                    <button onClick={() => stop(counterId)}>
+                        Stop
+                    </button>
+                    <button onClick={clear}>
+                        Clear
+                    </button>
                 </div>
             </>
         ) 
