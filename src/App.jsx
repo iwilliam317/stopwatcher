d12ddf2e4e6862c85a6b02a6f8b4cdbf44bf00d2
Shinji Obana <willshinji@gmail.com>
2020-12-17 21:27:28 +0000

    converting App from Class to Functional components using react-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React, {Component} from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css'
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 import { withAuthenticator, AmplifySignOut, AmplifyNav } from '@aws-amplify/ui-react'
 Amplify.configure(awsExports)
 
-const INITIAL_VALUES = {seconds: 0, minutes: 0, hours: 0, isStarted: false, counterId: 0} 
-class App extends Component {
-    
-    state = INITIAL_VALUES
-    
-    start = () => {
-        const {counter} = this
-        const {isStarted} = this.state
-        
-        if (isStarted) return        
-        let counterId = setInterval(() => counter(), 1000)
-        this.setState({...this.state, counterId})
+
+const App = () => {
+
+    const INITIAL_TIME = {seconds: 0, minutes: 0, hours: 0}
+
+    const [time, setTime] = useState(INITIAL_TIME)
+    const refTime = useRef(time)
+    refTime.current = time
+    const [isStarted, setIsStarted] = useState(false)
+    const [counterId, setCounterId] = useState(0)
+ 
+    const start = () => {
+        if (isStarted) return
+        let _counterId = setInterval(() => counter(), 1000)
+        setCounterId(_counterId)
     }
-    
-    counter = () => {
-        let {seconds, minutes, hours} = this.state
+
+    const counter = () => {
+        let { seconds, minutes, hours } = refTime.current
         seconds++
         if (seconds >= 60) {
             seconds = 0
@@ -30,54 +33,57 @@ class App extends Component {
                 hours++
             }
         }
-        this.setState({seconds, minutes, hours, isStarted: true})
+        setTime({seconds, minutes, hours})
+        setIsStarted(true)
     }
 
-    clear = () => {
-        const {stop} = this
-        const {counterId} = this.state
-
+    const clear = () => {
+        
         stop(counterId)
-        this.setState(INITIAL_VALUES)
+        setTime(INITIAL_TIME)
+        setIsStarted(false)
+        
     }
 
-    stop = counterId => {
-        clearInterval(counterId);
+    const stop = counterId => {
+        clearInterval(counterId)
+        setCounterId(0)
     }
 
-    formatTime = number => {
+    const formatTime = number => {
         return (number ? (number > 9 ? number : "0" + number) : "00")
     }
 
-    render () {
-        const {seconds, minutes, hours, isStarted, counterId} = this.state
-        const {start, stop, clear, formatTime} = this
-        return (
-            <>
-            
-                <div className='navbar'>
-                    <AmplifySignOut />
 
-                </div>
-                <div className='stopwatcher'>
-                    {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}
-                </div>
-                <div className='buttons'>
-                    <button onClick={start} disabled={isStarted}>
-                        Start
+    const { seconds, minutes, hours} = time
+   
+    return (
+        <>
+
+            <div className='navbar'>
+                <AmplifySignOut />
+
+            </div>
+            <div className='stopwatcher'>
+                {formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}
+            </div>
+            <div className='buttons'>
+                <button onClick={() => start()}>
+                    Start
                     </button>
-                    <button onClick={() => stop(counterId)}>
-                        Stop
+                    
+                <button onClick={() => stop(counterId)}>
+                    Stop
                     </button>
-                    <button onClick={clear}>
-                        Clear
+                <button onClick={clear}>
+                    Clear
                     </button>
-                </div>
-                <div className='rights'>
-                    <p><small>Created by <a href="https://github.com/iwilliam317">William Shinji</a></small></p>
-                </div>
-            </>
-        ) 
-    }
+            </div>
+            <div className='rights'>
+                <p><small>Created by <a href="https://github.com/iwilliam317">William Shinji</a></small></p>
+            </div>
+        </>
+    )
+
 }
 export default withAuthenticator(App)
